a1091a9a11f3f5cd8b7d7a398656130ec569de08
Васильев Николай Игоревич <nivasilev@mts.ru>
2022-05-16 15:18:26 +0300

    move properties to application.properties

--- src/main/java/ru/n1k0/swaggerexporter/config/ConnectionSettings.java
+++ src/main/java/ru/n1k0/swaggerexporter/config/ConnectionSettings.java
@@ -1,22 +1,15 @@
 package ru.n1k0.swaggerexporter.config;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
 
-//@ConfigurationProperties(prefix = "connection")
 @Component
 @Data
 public class ConnectionSettings {
-    Map<String, String> swaggerAddresses;
-    private String scrapeInterval;
 
-    public ConnectionSettings() {
-        swaggerAddresses = new HashMap<>();
-        swaggerAddresses.put("tvh-billing", "http://ingressinside.loadtest-tvh.mts-corp.ru/tvh-billing/desc/swagger.json");
-        swaggerAddresses.put("tvh-billing-v2", "http://ingressinside.loadtest-tvh.mts-corp.ru/tvh-billing-v2/swagger");
-    }
+    @Value("#{${swaggerUrls}}")
+    Map<String, String> swaggerAddresses;
 }
