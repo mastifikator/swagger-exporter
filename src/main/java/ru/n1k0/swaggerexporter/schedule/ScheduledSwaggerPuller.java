369d9abe59afc56c98246372de9c21617ec6e8f2
Васильев Николай Игоревич <nivasilev@mts.ru>
2022-05-17 15:43:54 +0300

    add unavailable services

--- src/main/java/ru/n1k0/swaggerexporter/schedule/ScheduledSwaggerPuller.java
+++ src/main/java/ru/n1k0/swaggerexporter/schedule/ScheduledSwaggerPuller.java
@@ -13,6 +13,11 @@ import ru.n1k0.swaggerexporter.config.ConnectionSettings;
 import ru.n1k0.swaggerexporter.repository.statusRepository;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -31,48 +36,69 @@ public class ScheduledSwaggerPuller {
     private MeterRegistry meterRegistry;
 
     @PostConstruct
-    public void initializeRepository(){
+    public void initializeRepository() {
         Map<String, String> swaggerAddresses = connectionSettings.getSwaggerAddresses();
 
-        for(Map.Entry<String, String> entry : swaggerAddresses.entrySet()){
+        for (Map.Entry<String, String> entry : swaggerAddresses.entrySet()) {
             statusRepository.save(entry.getKey(), new ArrayList<>());
         }
     }
 
     @Scheduled(initialDelay = 2000, fixedRateString = "${scrapeInterval}")
-    public void schedulePullSwaggerDocs(){
+    public void schedulePullSwaggerDocs() throws IOException {
         Map<String, String> swaggerAddresses = connectionSettings.getSwaggerAddresses();
 
-        for (Map.Entry<String, String> entry: swaggerAddresses.entrySet()){
-            SwaggerParseResult result = new OpenAPIParser().readLocation(entry.getValue(), null, null);
-                List<String> connectStatusList = statusRepository.findByService(entry.getKey());
+        for (Map.Entry<String, String> entry : swaggerAddresses.entrySet()) {
+            URL url = new URL(entry.getValue());
+            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+            int responseCode;
 
-                if(result.getOpenAPI() != null){
-                    OpenAPI openAPI = result.getOpenAPI();
-                    System.out.println("Get info from " + entry.getKey());
+            try{
+                responseCode = huc.getResponseCode();
+            }catch (ConnectException cex){
+                responseCode = 404;
+            }
 
-                    Gauge gauge = Gauge.builder("swagger_service_info", connectStatusList, List::size)
-                            .tag("service_name", entry.getKey())
-                            .tag("version", openAPI.getInfo().getVersion() != null ? openAPI.getInfo().getVersion() : "")
-                            .tag("title", openAPI.getInfo().getTitle() != null ? openAPI.getInfo().getTitle() : "")
-                            .tag("description", openAPI.getInfo().getDescription() != null ? openAPI.getInfo().getDescription() : "")
-                            .tag("paths", Integer.toString(openAPI.getPaths().size()))
-                            .description("Service Info from swagger documentation")
-                            .register(meterRegistry);
+            List<String> connectStatusList = statusRepository.findByService(entry.getKey());
 
-                    if(gauge.value() == 0){
-                        connectStatusList.add("connect");
-                    }
-                }else{
-                    Gauge gauge = meterRegistry.get("swagger_service_info").tag("service_name", entry.getKey()).gauge();
-                    if(gauge.value() == 1){
-                        connectStatusList.remove("connect");
-                    }
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                SwaggerParseResult result = new OpenAPIParser().readLocation(entry.getValue(), null, null);
+                OpenAPI openAPI = result.getOpenAPI();
+                System.out.println("Get info from " + entry.getKey());
+
+                Gauge gauge = Gauge.builder("swagger_service_info", connectStatusList, List::size)
+                        .tag("service_name", entry.getKey())
+                        .tag("version", openAPI.getInfo().getVersion() != null ? openAPI.getInfo().getVersion() : "")
+                        .tag("title", openAPI.getInfo().getTitle() != null ? openAPI.getInfo().getTitle() : "")
+                        .tag("description", openAPI.getInfo().getDescription() != null ? openAPI.getInfo().getDescription() : "")
+                        .tag("paths", Integer.toString(openAPI.getPaths().size()))
+                        .description("Service Info from swagger documentation")
+                        .register(meterRegistry);
+
+                if (gauge.value() == 0) {
+                    connectStatusList.add("connect");
                 }
+            } else {
+                System.out.println("Failed get info from " + entry.getKey());
 
-                if(meterRegistry.get("swagger_service_info").tag("service_name", entry.getKey()).gauges().size() > 1){
-                    meterRegistry.clear();
+                Gauge.builder("swagger_service_info", connectStatusList, List::size)
+                        .tag("service_name", entry.getKey())
+                        .tag("version", "?")
+                        .tag("title", entry.getKey())
+                        .tag("description", "Unavailable service")
+                        .tag("paths", "0")
+                        .description("Service Info unavailable")
+                        .register(meterRegistry);
+
+                Gauge existGauge = meterRegistry.get("swagger_service_info").tag("service_name", entry.getKey()).gauge();
+                if (existGauge.value() == 1) {
+                    connectStatusList.remove("connect");
                 }
+            }
+
+            if (meterRegistry.get("swagger_service_info").tag("service_name", entry.getKey()).gauges().size() > 1) {
+                meterRegistry.clear();
+            }
         }
     }
 }
